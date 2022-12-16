4ff02413a595c7da9d06de129f2fb89421a57f60
Tabi Idris <tabidrizy@gmail.com>
2022-12-16 11:09:16 +0100

    jasmine setup and teardown exercise

---
+++ jasmine-testing/exercise/setup-teardown/src/tests/mainSpec.ts
@@ -0,0 +1,12 @@
+import newArr from '../main';
+
+describe('newArr should add a new item to the start of array', () => {
+    const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+
+    it('should make a new array containing dog', () => {
+        expect(newArr(3, wordArr)).toContain('dog');
+    });
+    it('make a new array containing 3', () => {
+        expect(newArr(3, wordArr)).toContain(3);
+    });
+});
